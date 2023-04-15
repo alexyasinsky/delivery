368c612a277f3336c991e58743f38ffcdae2f7e9
alexyasinsky <alex_yasinsky@mail.ru>
2023-04-15 19:59:56 +0300

    feedback carousel added

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,29 +1,75 @@
 <script setup>
 import VButton from './VButton.vue';
 import FeedbackForm from './FeedbackForm.vue';
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide } from 'vue3-carousel'
 
 const steps = [
   {
     image: 'step1',
-    highline: 'Шаг 1',
+    title: 'Шаг 1',
     text: 'Придумайте, что и кому отправить',
   },
   {
     image: 'step2',
-    highline: 'Шаг 2',
+    title: 'Шаг 2',
     text: 'Создайте заявку на сайте SendIt - Ваш персональный менеджер позвонит и подтвердит запрос, уточнит все необходимые данные',
   },
   {
     image: 'step3',
-    highline: 'Шаг 3',
+    title: 'Шаг 3',
     text: 'К Вам приезжает курьер, забирает посылку, Вы подписываете один документ',
   },
   {
     image: 'step4',
-    highline: 'Шаг 4',
+    title: 'Шаг 4',
     text: 'Вы ждёте подтверждения доставки, слова радости и благодарности от адресата :)',
   },
 ];
+
+const feedbacks = [
+  {
+    id: 'feedback00001',
+    image: 'feedback1',
+    name: 'Дарья',
+    company: 'La Intuicion',
+    package: 'товары',
+    departure: 'Москва',
+    destination: 'Нижневартовск',
+    text: 'Отличный и качественный сервис. Долго искали курьерскую службу для доставки наших товаров. Решили попробовать и были приятно удивлены, что помимо удобного сайта, компания предоставляет доступ в личный кабинет, где реализованы многие наши потребности (ведение адресной базы, выбор и вызов курьера в пару кликов).'
+  },
+  {
+    id: 'feedback00002',
+    image: 'feedback2',
+    name: 'Елена',
+    company: 'Global Logic',
+    package: 'товары',
+    departure: 'Москва',
+    destination: 'Калининград',
+    text: 'Мне нравится работать с SendIt, потому что это удобно: моментальный расчет стоимости доставки, удобный сайт, выборка среди основных надёжных логистических компаний и самый значимый фактор – всё это можно оперативно организовать и не волноваться. И ещё хочу сказать отдельное спасибо менеджеру за отзывчивость.'
+  }
+]
+
+const feedbackCarouselBreakpoints = {
+  320: {
+    itemsToShow: 1,
+  },
+  768: {
+    itemsToShow: 2,
+  },
+};
+
+import {ref} from 'vue'
+const feedbackCarousel = ref(null);
+
+function nextFeedbackSlide() {
+  return feedbackCarousel.value.next();
+}
+
+function previousFeedBackSlide() {
+  return feedbackCarousel.value.prev();
+}
+
 </script>
 
 <template>
@@ -43,7 +89,7 @@ const steps = [
     <section class="step__box padding-site">
       <div class="step" v-for="(step, ind) in steps" :key="ind">
         <img :src="`src/assets/img/${step.image}.png`" :alt="step.image" />
-        <h3>{{ step.highline }}</h3>
+        <h3>{{ step.title }}</h3>
         <p>{{ step.text }}</p>
       </div>
     </section>
@@ -63,10 +109,38 @@ const steps = [
         color-style="blue-yellow"
       />
     </section>
+    <section class="feedback__box padding-site">
+        <h1>ОТЗЫВЫ</h1>
+      <div class="feedback__carousel">
+        <button class="feedback__button feedback__button_left" @click="nextFeedbackSlide">❮</button>
+        <button class="feedback__button feedback__button_right" @click="previousFeedBackSlide">❯</button>
+        <carousel
+            :wrap-around="true"
+            snapAlign="start"
+            :breakpoints="feedbackCarouselBreakpoints"
+            ref="feedbackCarousel"
+        >
+          <slide v-for="feedback in feedbacks" :key="feedback.id">
+            <div class="feedback">
+              <div>
+                <img :src="`src/assets/img/${feedback.image}.png`" :alt="feedback.image">
+                <h5 class="feedback__name" >{{ feedback.name }}</h5>
+                <h6 class="feedback__company">{{ feedback.company }}</h6>
+                <p class="feedback__desc">Посылка: {{ feedback.package }}</p>
+                <p class="feedback__desc">Откуда: {{ feedback.departure }}</p>
+                <p class="feedback__desc">Куда: {{ feedback.destination }}</p>
+              </div>
+              <p class="feedback__text">{{ feedback.text }}</p>
+            </div>
+          </slide>
+        </carousel>
+      </div>
+    </section>
   </main>
 </template>
 
 <style lang="scss" scoped>
+
 .info {
   background-color: #f7f7f7;
   img {
@@ -105,6 +179,29 @@ const steps = [
     color: #ffffff;
   }
 }
+
+.feedback {
+  &__carousel {
+    position: relative;
+  }
+  &__button {
+    position: absolute;
+    color: #B3B3B3;
+    z-index: 3;
+    background: 0;
+    &_left {
+      left: 0;
+    }
+    &_right {
+      right: 0;
+    }
+  }
+
+  img {
+    float: left;
+  }
+}
+
 
 @media (min-width: 320px) {
   .info {
@@ -170,6 +267,47 @@ const steps = [
       font-size: 14px;
       line-height: 13px;
       margin-bottom: 14px;
+    }
+  }
+
+  .feedback {
+    text-align: left;
+    padding: 0 25px;
+    max-width: 500px;
+    &__box {
+      padding-top: 21px;
+      padding-bottom: 21px;
+      h1 {
+        font-size: 28px;
+        margin-bottom: 20px;
+      }
+    }
+    &__button {
+      &_left, &_right {
+        top: 100px;
+        font-size: 30px;
+      }
+    }
+    img {
+      width: 100px;
+      margin-right: 20px;
+    }
+    &__name {
+      margin-top: -4px;
+      font-size: 26px;
+    }
+    &__company {
+      margin: 10px 0;
+      font-size: 18px;
+    }
+    &__desc {
+      font-size: 10px;
+      margin-bottom: 5px;
+    }
+    &__text {
+      margin-top: 10px;
+      font-size: 12px;
+      line-height: 14px;
     }
   }
 }
@@ -250,6 +388,30 @@ const steps = [
       margin-right: 130px;
     }
   }
+
+  .feedback {
+    padding: 0 36px;
+    max-width: 500px;
+    &__box {
+      padding-top: 52px;
+      padding-bottom: 52px;
+      h1 {
+        font-size: 36px;
+        margin-bottom: 30px;
+      }
+    }
+    &__button {
+      &_left, &__right {
+        font-size: 30px;
+      }
+    }
+
+    &__text {
+      font-size: 14px;
+      line-height: 17px;
+    }
+  }
+
 }
 
 @media (min-width: 1440px) {
@@ -314,6 +476,48 @@ const steps = [
     p {
       font-size: 24px;
       line-height: 23px;
+    }
+  }
+
+  .feedback {
+    padding: 0 100px;
+    max-width: 800px;
+    transition: left 1s, visibility 0.4s;
+    &__box {
+      padding-top: 69px;
+      padding-bottom: 69px;
+    }
+    &__button {
+      &_left, &_right {
+        top: 130px;
+        font-size: 50px;
+      }
+    }
+
+    &__text {
+      font-size: 14px;
+      line-height: 17px;
+    }
+    img {
+      width: 150px;
+      margin-right: 30px;
+    }
+    &__name {
+      margin-top: -4px;
+      font-size: 36px;
+    }
+    &__company {
+      margin: 15px 0;
+      font-size: 24px;
+    }
+    &__desc {
+      font-size: 16px;
+      margin-bottom: 5px;
+    }
+    &__text {
+      margin-top: 30px;
+      font-size: 16px;
+      line-height: 19px;
     }
   }
 }
